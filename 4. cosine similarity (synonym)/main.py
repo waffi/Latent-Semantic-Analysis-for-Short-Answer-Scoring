27067fbdb7cb6b8f75dd241f31fa9fbc5f88159d
Waffi Fatur Rahman <waffifaturrahman@yahoo.co.id>
2018-07-28 16:24:33 +0700

    no message

--- 4. cosine similarity (synonym)/main.py
+++ 4. cosine similarity (synonym)/main.py
@@ -11,7 +11,7 @@ source_path = 'source/' + str(source) + '/'
 
 #get svd
 term_list = load(source_path + 'term_list.npy')
-# print term_list.shape
+term_list = term_list.tolist()
 
 #get svd
 
@@ -34,26 +34,26 @@ svd_midf.setReductionSVD(k_dimension)
 m1 = zeros([len(term_list), 7])	
 m2 = zeros([len(term_list), 7])	
 
-m1[991,0]=1
-m2[68,0]=1
+m1[term_list.index("softwar"),0]=1
+m2[term_list.index("program"),0]=1
 
-m1[68,1]=1
-m2[1202,1]=1
+m1[term_list.index("program"),1]=1
+m2[term_list.index("applic"),1]=1
 
-m1[1202,2]=1
-m2[991,2]=1
+m1[term_list.index("applic"),2]=1
+m2[term_list.index("softwar"),2]=1
 
-m1[418,3]=1
-m2[619,3]=1
+m1[term_list.index("enhanc"),3]=1
+m2[term_list.index("improv"),3]=1
 
-m1[989,4]=1
-m2[255,4]=1
+m1[term_list.index("product"),4]=1
+m2[term_list.index("construct"),4]=1
 
-m1[539,5]=1
-m2[800,5]=1
+m1[term_list.index("function"),5]=1
+m2[term_list.index("method"),5]=1
 
-m1[188,6]=1
-m2[777,6]=1
+m1[term_list.index("chang"),6]=1
+m2[term_list.index("manipul"),6]=1
 
 for i in range(1, 8):
 	
